b7dc8f614f49ea7b97a15bbee3d60802d0ccd622
Kumar Turlapati <kumar.turlapati@popcornapps.com>
2020-06-22 12:57:03 +0530

    added ga account

--- src/Layout/partials/head.tpl.php
+++ src/Layout/partials/head.tpl.php
@@ -1,14 +1,23 @@
-  <!-- meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Basic Google fonts start here -->
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-  <!-- Bootstrap 4.4.1 framwork styles -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <!-- card animation styles -->
-  <link rel="stylesheet" href="/assets/css/aos.css">
-  <link rel="stylesheet" href="/assets/css/fancybox.min.css">
-  <!-- custom styles -->
-  <link rel="stylesheet" type="text/css" href="/assets/css/custom-styles.css">
-  <link rel="icon" href="/images/favicon.ico" />
-  <title><?php echo $page_title ?></title>
+<!-- meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- Basic Google fonts start here -->
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+<!-- Bootstrap 4.4.1 framwork styles -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!-- card animation styles -->
+<link rel="stylesheet" href="/assets/css/aos.css">
+<link rel="stylesheet" href="/assets/css/fancybox.min.css">
+<!-- custom styles -->
+<link rel="stylesheet" type="text/css" href="/assets/css/custom-styles.css">
+<link rel="icon" href="/images/favicon.ico" />
+<title><?php echo $page_title ?></title>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-170212035-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-170212035-1');
+</script>
+
